7af430cc7e2e407678ae4551e8e4b1a275fb2135
VayerMaking <martin.vayer@gmail.com>
2020-10-01 13:26:53 +0300

    asdsf

---
+++ os/01102020/01102020.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+size_t strlen( const char * );
+size_t strlen(const char * input){
+  int size;
+  for(int i = 0;i<size_of(input);i++){
+    size = i;
+  }
+  return size;
+}
+
+int main(){
+  printf("%d\n", size_t strlen("asdf"));
+  return 0;
+}
