b3c98959f0d254bb92402a5f28096ffc8dc92a52
VayerMaking <martin.vayer@gmail.com>
2020-10-16 11:54:36 +0300

    std logger

--- rs/16102020/logger.py
+++ rs/16102020/logger.py
@@ -1,5 +1,11 @@
 class Logger:
     def log(self, msg):
+        raise NotImplementedError("neshtasnik")
+        #print(msg)
+class StdOutLogger(Logger):
+    def log(self, msg):
         print(msg)
-logger = Logger()
-logger.log("asdf")
+#logger = Logger()
+logger2 = StdOutLogger()
+logger2.log("qwerty")
+#logger.log("asdf")
