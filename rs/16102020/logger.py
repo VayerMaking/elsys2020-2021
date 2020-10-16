1f0654147c0315fd6af79c12f49c97fdf8413917
VayerMaking <martin.vayer@gmail.com>
2020-10-16 12:16:06 +0300

    file logger

--- rs/16102020/logger.py
+++ rs/16102020/logger.py
@@ -5,7 +5,18 @@ class Logger:
 class StdOutLogger(Logger):
     def log(self, msg):
         print(msg)
+class FileLogger(Logger):
+    def __init__(self, filename):
+        self.file = open(filename, 'a')
+
+    def log(self, msg):
+        self.file.write(msg)
+
+    def __del__(self):
+        self.file.close()
 #logger = Logger()
 logger2 = StdOutLogger()
+logger3 = FileLogger("log.txt")
+logger3.log("234\n")
 logger2.log("qwerty")
 #logger.log("asdf")
