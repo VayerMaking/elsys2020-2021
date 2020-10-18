02e2e262a53b7b525d9eeb89f3ce8dad2fbacc4a
VayerMaking <martin.vayer@gmail.com>
2020-10-18 18:03:26 +0300

    fixing stupid mistake

--- rs/hw1/main.py
+++ rs/hw1/main.py
@@ -4,8 +4,8 @@ def exchange(bgn_amount, currency, exchange_date):
     if(currency == 'EUR'):
         result = bgn_amount * 0.511292
     elif(currency == 'TRY'):
-        result = bgn_amount * (1.46 + (0.0014 * (date.today() - exchange_date).days))
+        result = bgn_amount * (1.46 + (0.0014 * (exchange_date - date(2014, 8, 28)).days))
     return result
-    
+
 print(exchange(2, "EUR", date(2020, 10, 14)))
 print(exchange(2, "TRY", date(2020, 10, 14)))
