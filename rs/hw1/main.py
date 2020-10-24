e56e53d16af36e366a58ed461e118e978e8184b1
VayerMaking <martin.vayer@gmail.com>
2020-10-24 13:17:27 +0300

    finished filter_logs

--- rs/hw1/main.py
+++ rs/hw1/main.py
@@ -1,11 +1,14 @@
-from datetime import date
+logs = [
+    {"timestamp": "2020-05-11T13:42:50", "status": "error", "countryISO2": "BG"},
+    {"timestamp": "2020-05-11T13:43:20", "status": "success", "countryISO2": "UK"},
+    {"timestamp": "2020-05-11T13:44:30", "status": "success", "countryISO2": "NZ"},
+]
 
-def exchange(bgn_amount, currency, exchange_date):
-    if(currency == 'EUR'):
-        result = bgn_amount * 0.511292
-    elif(currency == 'TRY'):
-        result = bgn_amount * (1.46 + (0.0014 * (exchange_date - date(2014, 8, 28)).days))
-    return result
 
-print(exchange(2, "EUR", date(2020, 10, 14)))
-print(exchange(2, "TRY", date(2020, 10, 14)))
+def filter_logs(logs, key, value):
+    filtered_log = [d for d in logs if d[key] in value]
+    return filtered_log
+
+
+
+print(filter_logs(logs, "status","error"))
