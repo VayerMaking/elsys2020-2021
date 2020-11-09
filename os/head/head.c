fa12f602cea8c9a400314e4bd214260d67c538dd
VayerMaking <martin.vayer@gmail.com>
2020-11-09 16:02:32 +0200

    almost

--- os/head/head.c
+++ os/head/head.c
@@ -3,12 +3,13 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-#define BUFFER_SIZE 50
+#define BUFFER_SIZE 1024
 
 int main(){
   int   file;
   char  buffer[BUFFER_SIZE + 1] = {0};
   int   read_size;
+  int count;
 
   file = open("a.txt", O_RDONLY);
   if (file == -1){
@@ -16,12 +17,21 @@ int main(){
 
   }
   while ((read_size = read(file, buffer, BUFFER_SIZE)) > 0){
-    write(1, &buffer, read_size);
-  }
-  //while((read_size = read(file, buffer, BUFFER_SIZE)) > 0){
-  //  buffer[];
+    int i = 0;
+    while(i < read_size){
+      if(count >= 10){
+        break;
+      }else{
+        if(buffer[i] == '\n'){
+          count++;
+        }
+      }
+        i++;
+    }
 
-  //}
+  }
+  write(1, buffer, read_size);
+
   close(file);
   return (0);
 }
