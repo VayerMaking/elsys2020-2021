43a5827409cb82d99a6350ba3e70fe5a8eb54466
VayerMaking <martin.vayer@gmail.com>
2020-11-29 15:08:33 +0200

    nqkvi sMoL edits

--- os/tail/tail7.c
+++ os/tail/tail7.c
@@ -6,72 +6,63 @@
 #include <sys/types.h>
 
 #define BUFFER_SIZE 100
-
-int main(int argc, char *argv[])
-{
-    if (argc < 2)
-    {
-        fprintf(stderr, "open() failed");
-    }
-    int file = open(argv[1], O_RDONLY);
-    if (file == -1)
-    {
-        char temp[10];
-        strcpy(temp, argv[1]);
-        int i = 0, size = 0;
-        while (temp[i] != '\0')
-        {
-            size++;
-            i++;
-        }
-        if (temp[0] == '/' && temp[size - 1] == '/')
-        {
-            fprintf(stderr, "tail: error reading '%s': Its a directory");
-        }
-        else if (temp[0]==">")
-        {
-            fprintf(stderr,"tail: error writing 'standard output': No space left on device");
-        }
-
-        else
-        {
-            fprintf(stderr, "tail: cannot open '%s' for reading: ", argv[1]);
-            perror("");
-        }
-    }
-
+void tail(char *filename){
+    int file = open(filename, O_RDONLY);
     char buffer[BUFFER_SIZE + 1] = {0};
     int symbols = read(file, buffer, BUFFER_SIZE);
     int counter = 0;
-    if (symbols > 0)
-    {
+    if (symbols > 0){
         int position = symbols;
-        while (position > 0)
-        {
-            if (buffer[position] == '\n')
-            {
+        while (position > 0){
+            if (buffer[position] == '\n'){
                 counter++;
             }
-            if (counter == 10)
-            {
+            if (counter == 10){
                 break;
             }
             position--;
         }
 
         int temp = 0;
-        if (position != 0)
-        {
+        if (position != 0){
             temp = position + 1;
-        } //to skip the new line
-        //printf("%d, %d\n", position, lseek(file,position,SEEK_SET)); //just to check
+        }
+        //printf("%d, %d, %d\n", position, lseek(file,position,SEEK_SET),symbols); //just to check
         char result[BUFFER_SIZE] = {0};
-        for (int i = 0; i < BUFFER_SIZE - position - 1; i++)
-        {
+        for (int i = 0; i < BUFFER_SIZE - position - 1; i++){
             result[i] = buffer[temp + i];
         }
         write(1, &result, BUFFER_SIZE);
     }
+    write(1,"\n",1);
     close(file);
+}
+void pretty_print(char *filename){
+    write(1, "==> ", 5);
+    write(1, filename, strlen(filename));
+    write(1, " <==\n", 6);
+}
+int main(int argc, char *argv[]){
+    if (argc < 2){
+        char err[100];
+        strcat(err, "error opening");
+        strcat(err, argv[1]);
+        printf("%s\n", err);
+        perror(err);
+    }else if (argc > 2){
+        int i = 1;
+        while (i<argc){
+            if(i!=1){
+                write(1,"\n",1);
+            }
+            pretty_print(argv[i]);
+            tail(argv[i]);
+            i++;
+        }
+    }else{
+        //pretty_print(argv[1]);
+        tail(argv[1]);
+    }
+
     return 0;
 }
