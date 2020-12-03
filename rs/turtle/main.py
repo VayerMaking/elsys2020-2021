2f1fea5c37db61266510e7fe91aca95a24dfd0b4
VayerMaking <martin.vayer@gmail.com>
2020-12-03 20:00:01 +0200

    working move

--- rs/turtle/main.py
+++ rs/turtle/main.py
@@ -3,7 +3,7 @@ class Turtle():
         self.rows = rows
         self.columns = columns
         self.canvas = []
-        self.canvas = [[0 for i in range(columns)] for j in range(rows)]
+        self.canvas = [[0 for i in range(rows)] for j in range(columns)]
         self.orientation = 0
         self.curr_pos_x = 0
         self.curr_pos_y = 0
@@ -12,7 +12,7 @@ class Turtle():
         print("rows: {}, columns: {}".format(self.rows, self.columns))
 
     def spawn_at(self, rows, columns):
-        self.canvas[columns][rows] = self.canvas[columns][rows] + 1
+        self.canvas[rows][columns] = self.canvas[rows][columns] + 1
         self.curr_pos_x = rows
         self.curr_pos_y = columns
 
@@ -25,19 +25,34 @@ class Turtle():
     def check_orientation(self):
         return abs(self.orientation/90)
 
+
+
     def move(self):
         if(abs(self.orientation/90) == 0):
             print("nadqsno gleda")
-            self.curr_pos_x += 1
+            self.curr_pos_y += 1
+            if self.curr_pos_y + 1 > self.rows:
+                self.curr_pos_y = 0
+
         elif(abs(self.orientation/90) == 1):
             print("nadolu gleda")
-            self.curr_pos_y += 1
+            self.curr_pos_x += 1
+            if self.curr_pos_x + 1 > self.columns:
+                self.curr_pos_x = 0
+
         elif (abs(self.orientation/90) == 2):
             print("nalqwo gleda")
-            self.curr_pos_x -= 1
+            self.curr_pos_y -= 1
+            if self.curr_pos_y + 1 > self.rows:
+                self.curr_pos_y = 0
+
         elif (abs(self.orientation/90) == 3):
             print("nagore gleda")
-            self.curr_pos_y -= 1
+            self.curr_pos_x -= 1
+            if self.curr_pos_x + 1 > self.columns:
+                self.curr_pos_x = 0
+
+        self.canvas[self.curr_pos_x][self.curr_pos_y] += 1
 
     def reset_if_360(self):
         if(self.orientation == 360):
@@ -60,9 +75,15 @@ turtle = Turtle(3,3)
 turtle.spawn_at(0,0)
 turtle.print_rows_columns()
 turtle.print_turtle()
+turtle.turn_right()
+turtle.print_turtle()
 turtle.move()
 turtle.print_turtle()
-
+turtle.move()
+turtle.print_turtle()
+turtle.move()
+turtle.print_turtle()
+'''
 print("default orientation", turtle.orientation)
 turtle.turn_left()
 print("changed orientation", turtle.orientation)
@@ -80,7 +101,7 @@ turtle.turn_right()
 print("changed orientation", turtle.orientation)
 turtle.turn_right()
 print("changed orientation", turtle.orientation)
-
+'''
 print(turtle.check_orientation())
 turtle.move()
 
