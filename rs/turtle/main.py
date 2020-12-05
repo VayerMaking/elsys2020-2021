d2438f9e27fe15b64d81b72d89bb8f7e4f4c6885
VayerMaking <martin.vayer@gmail.com>
2020-12-05 23:42:02 +0200

    финал

--- rs/turtle/main.py
+++ rs/turtle/main.py
@@ -3,8 +3,7 @@ from turtle import SimpleCanvas
 
 turtle = Turtle(3,3)
 turtle.spawn_at(0,0)
-turtle.print_rows_columns()
-turtle.print_turtle()
+
 turtle.turn_right()
 turtle.print_turtle()
 turtle.move()
@@ -32,7 +31,6 @@ print("changed orientation", turtle.orientation)
 turtle.turn_right()
 print("changed orientation", turtle.orientation)
 '''
-print(turtle.check_orientation())
 turtle.move()
 
 print("turtle curr_pos_x : {}, turtle curr_pos_y : {}".format(turtle.curr_pos_x, turtle.curr_pos_y))
--- rs/turtle/turtle.py
+++ rs/turtle/turtle.py
@@ -8,62 +8,36 @@ class Turtle():
         self.curr_pos_x = None
         self.curr_pos_y = None
 
-    def print_rows_columns(self):
-        print("rows: {}, columns: {}".format(self.rows, self.columns))
-
     def spawn_at(self, rows, columns):
         self.canvas[rows][columns] = self.canvas[rows][columns] + 1
         self.curr_pos_x = rows
         self.curr_pos_y = columns
-
-    def print_turtle(self):
-        print("##################")
-        for list in self.canvas:
-            print(list)
-        print("##################")
-
-    def check_orientation(self):
-        return abs(self.orientation/90)
-
-        # 0 = nadqsno
-        # 1 = nadolu
-        # 2 = nalqwo
-        # 3 = nagore
 
     def move(self):
         if self.curr_pos_y == None or self.curr_pos_x == None:
             raise RuntimeError("you have to spawn the turtle first")
 
         if(self.orientation == 0):
-            #print("nadqsno gleda")
             self.curr_pos_y += 1
             if self.curr_pos_y + 1 > self.rows:
                 self.curr_pos_y = 0
 
         elif(self.orientation == 1):
-            #print("nadolu gleda")
             self.curr_pos_x += 1
             if self.curr_pos_x + 1 > self.columns:
                 self.curr_pos_x = 0
 
         elif(self.orientation == 2):
-            #print("nalqwo gleda")
             self.curr_pos_y -= 1
             if self.curr_pos_y + 1 > self.rows:
                 self.curr_pos_y = 0
 
         elif(self.orientation == 3):
-            #print("nagore gleda")
             self.curr_pos_x -= 1
             if self.curr_pos_x + 1 > self.columns:
                 self.curr_pos_x = 0
 
         self.canvas[self.curr_pos_x][self.curr_pos_y] += 1
-
-    def reset_if_360(self):
-        if(self.orientation == 360):
-            self.orientation = 0
-
 
     def turn_right(self):
         self.orientation += 1
@@ -77,28 +51,30 @@ class Turtle():
 
 class SimpleCanvas():
     def __init__(self, canvas, symbol_list):
+        self.pixel = []
         self.canvas = canvas
         self.symbol_list = symbol_list
+
     def draw(self):
-        print("draw canvas",self.canvas)
         maxx = max(map(max, self.canvas))
         intensity = 0
+        res = ""
+        self.pixel = [[0 for i in range(len(self.canvas))] for j in range(len(self.canvas[0]))]
+
         for row in range(len(self.canvas)):
             for column in range(len(self.canvas[row])):
                 intensity = self.canvas[row][column] / maxx
-                print("row cloomn",type(self.canvas[row][column]))
-                print("maxx: ",type(maxx))
-
                 if intensity == 0:
-                    self.canvas[row][column] = self.symbol_list[0]
+                    self.pixel[row][column] = self.symbol_list[0]
                 elif intensity > 0 and intensity <= 0.3:
-                    self.canvas[row][column] = self.symbol_list[1]
-                elif intensity > 0.3 and intensity <= 0.6:
-                    self.canvas[row][column] = self.symbol_list[2]
-                elif intensity > 0.6 and intensity <= 1:
-                    self.canvas[row][column] = self.symbol_list[3]
+                    self.pixel[row][column] = self.symbol_list[1]
+                elif intensity > 0.3 and intensity <= 0.6 and self.symbol_list[2]:
+                    self.pixel[row][column] = self.symbol_list[2]
+                elif intensity > 0.6 and intensity <= 1 and self.symbol_list[3]:
+                    self.pixel[row][column] = self.symbol_list[3]
 
-
-        #print(self.canvas)
-
-        return self.canvas
+        string = [0 for i in range(len(self.canvas))]
+        for i in range(len(self.canvas)):
+            string[i]= ''.join(self.pixel[i])
+        res = "\n".join(string)
+        return res
