ebe0a98d532d6470b65d1410b513806a2b3339bd
VayerMaking <martin.vayer@gmail.com>
2021-01-26 15:22:32 +0200

    not working main

--- oop/hw2/main.cpp
+++ oop/hw2/main.cpp
@@ -7,6 +7,39 @@
 #include "software.h"
 
 int main(){
+/*
+    GameTorrent game_torrent = GameTorrent("CS:GO", 120, "Valve", 120000, "Linux + Proton", 'M');
+		MovieTorrent movie_torrent = MovieTorrent("Star Wars: The Empire Strikes Back", 30, "Lucas", 20000, "spilberg?", 180, "English");
+		SoftwareTorrent software_torrent = SoftwareTorrent("DB Browser", 20, "open source", 1000, "open source", "Linux", 4, 1, 3);
+
+
+		GameTorrent game_torrent_2 = GameTorrent("Crysis", 80, "assd", 1200000, "Windows", 'M');
+		MovieTorrent movie_torrent_2 = MovieTorrent("Indiana Jones: Riders of The Lost Ark", 10, "asdf", 9000, "af asd", 150, "English");
+		SoftwareTorrent software_torrent_2 = SoftwareTorrent("Atom", 30, "github", 100, "github", "Linux", 1, 0, 9);
+
+
+		GameTorrent game_torrent_3 = GameTorrent("Cyberpunk 2077", 90, "qwerrt", 400000, "Windows", 'P');
+		MovieTorrent movie_torrent_3 = MovieTorrent("Transformers", 30, "asdff", 20000, "Spilberg", 120, "English");
+		SoftwareTorrent software_torrent_3 = SoftwareTorrent("Firefox", 40, "Firefox", 11000, "Firefox", "Linux", 3, 0, 6);
+
+
+		vector<GameTorrent*> game_torrents;
+		vector<MovieTorrent*> movie_torrents;
+		vector<SoftwareTorrent*> software_torrents;
+
+
+		game_torrents.push_back(&game_torrent);
+		game_torrents.push_back(&game_torrent_2);
+		game_torrents.push_back(&game_torrent_3);
+
+		movie_torrents.push_back(&movie_torrent);
+		movie_torrents.push_back(&movie_torrent_2);
+		movie_torrents.push_back(&movie_torrent_3);
+
+		software_torrents.push_back(&software_torrent);
+		software_torrents.push_back(&software_torrent_2);
+		software_torrents.push_back(&software_torrent_3);
+*/
 
   return 0;
 }
